1db5656011f8a74eb281506b756d9303f8c73992
Karina Valens <karyvalens@yahoo.com>
2022-06-04 01:21:30 +0200

    carrusel

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'center',
+    contain: true,
+    groupCells: true,
+    pageDots: false
+});
+
+// element argument can be a selector string
+//   for an individual element
+//var flkty = new Flickity( '.main-carousel', {
+// options
+//});
