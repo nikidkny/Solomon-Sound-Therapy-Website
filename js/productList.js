564089dd67a8c1605ac8eee02025d65191bcc27c
Egill Þór <egillthor123@gmail.com>
2022-06-05 10:34:41 +0200

    footer fix

--- js/productList.js
+++ js/productList.js
@@ -14,9 +14,9 @@ fetch(url, options)
     return response.json();
   })
   .then((data) => {
-    //to check if we have the data
+    //call function to populate correct data
     handleData(data);
-    //hide spinner when we get the posters
+    //hide spinner when we get the data
     let spinner = document.querySelector(".spinner");
     spinner.classList.add("hide");
   })
@@ -31,45 +31,27 @@ function handleData(sessions) {
 
     // 2. Clone template
     const clone = template.cloneNode(true);
-    // 4. Populated with data
 
+    // 4. Populated with data
     clone.querySelector("img").src = session.SessionImage;
     clone.querySelector(".product-price").textContent =
       session.SessionPrice + " kr.";
-
     clone.querySelector(".product-duration").textContent =
       session.SessionLength;
-
     clone.querySelector(".product-title").textContent = session.TherapyName;
     clone.querySelector(".product-methodology").textContent =
       session.Methodology + " Session";
     clone.querySelector(".product-description").textContent =
       session.ShortDescription;
-
     clone.querySelector(
       ".book-now-button-small"
     ).innerHTML = `BOOK NOW <i class="fa-solid fa-chevron-right"></i>`;
-
     clone
       .querySelector(".book-now-button-small")
       .setAttribute("href", `product.html?id=${session._id}`);
-
     clone
       .querySelector("a")
       .setAttribute("href", `product.html?id=${session._id}`);
-
-    /*
-    clone
-      .querySelector("a")
-      .setAttribute("href", `product.html?id=${soap._id}`);
-
-    clone.querySelector(".producTitle").textContent = soap.productname;
-    clone.querySelector(".desc").textContent = soap.productgroup;
-    clone.querySelector(".price").textContent = soap.price;
-    clone.querySelector("img").src = soap.img_url;
-    clone.querySelector("img").alt = soap.productname;
-
-    */
 
     // 5. Grap the parent
     const parent = document.querySelector("#product-list-container");
